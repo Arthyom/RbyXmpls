31fd79cb8d851aff2a5bb91719af3ea050a1feec
Arthyom <allpipiasaaa@hotmail.com>
2016-01-26 20:17:22 -0600

    problemas barcos que te hacen cada vez menos barco

---
+++ Xmpl.rb
@@ -0,0 +1,15 @@
+# big num
+puts 3<<4
+puts 4>>3
+puts 10.div 2 
+puts 10.equal? 4
+puts 25.even? 
+puts 12.3.fdiv 23
+puts 12.odd? 
+puts 4.remainder 3
+puts 2.div 2
+
+# integer 
+puts 4.rationalize 
+puts 3.1203232323.round 2
+puts 3.times 
---
+++ matrices.rb
@@ -0,0 +1,8 @@
+numeroLimite = 9#gets.chomp.to_i
+
+for linea in 1..numeroLimite
+	
+	(1..linea).each{|elemnt| print elemnt}
+	puts
+end
+
